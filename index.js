71a76930609d9a422eb32cd639be82d9887228b1
Karan <rkrana811@gmail.com>
2022-10-27 01:07:50 -0500

    styled html; fixed js

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ var notesOutput = document.querySelectorAll(".no-of-notes");
 var show = document.querySelector(".hiddenContainer");
 var proceed = document.querySelector(".proceed");
 var initialMessage = document.querySelector(".initialMessage");
+var footer = document.querySelector(".footer");
 
 var notes = [2000, 500, 100, 20, 10, 5, 1];
 
 show.style.display = "none";
 
+footer.style.position = "absolute";
+
+
 proceed.addEventListener("click", function showDiv() {
-    if (billAmount.value > 0) {
+    console.log(typeof Number(billAmount.value))
+    if (Number(billAmount.value) > 0) {
         show.style.display = "block";
         initialMessage.style.display = "none";
         proceed.style.display = "none";
+        footer.style.position = "static";
     } else {
         initialMessage.innerHTML = ("Invalid Bill Amount")
     }
@@ -23,21 +29,16 @@ proceed.addEventListener("click", function showDiv() {
 
 
 checkButton.addEventListener("click", function validateBillAmt() {
-    if (billAmount.value > 0) {
-        show.style.display = "block";
-        console.log(cashGiven.value);
-        console.log(billAmount.value);
-        if (cashGiven.value >= billAmount.value) {
-            var returnChange = cashGiven.value - billAmount.value;
-            message.innerHTML = ("Change to be returned: " + returnChange);
-            calculateNotes(returnChange);
-        } else {
-            message.innerHTML = ("Invalid Amount")
-        }
+    if (Number(cashGiven.value) > Number(billAmount.value)) {
+        console.log('true')
+        var returnChange = Number(cashGiven.value) - Number(billAmount.value);
+        message.innerHTML = ("Change to be returned: " + returnChange);
+        calculateNotes(returnChange);
     } else {
-        message.innerHTML = ("Invalid Amount");
+        message.innerHTML = ("Invalid Amount")
     }
-});
+}
+);
 
 function calculateNotes(rtnchg) {
     for (var i = 0; i < notes.length; i++) {
